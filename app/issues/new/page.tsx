56b7f8094c945a65d9e9de69cafb26e030903804
minhasaatish <minhasaatish@gmail.com>
2024-01-30 09:20:25 +0530

    refactor: handle submit code

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -28,6 +28,18 @@ const NewIssuePage = () => {
   const [error, setError] = useState("");
   const [isFormSubmitting, setFormSubmitting] = useState(false);
 
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      setFormSubmitting(true);
+      await axios.post("/api/issues", data);
+      router.push("/issues");
+      setError("");
+    } catch (error) {
+      setFormSubmitting(false);
+      setError("An unexpected error occurs due to bad request.");
+    }
+  });
+
   return (
     <div className="max-w-xl">
       {error && (
@@ -35,20 +47,7 @@ const NewIssuePage = () => {
           <Callout.Text>{error}</Callout.Text>
         </Callout.Root>
       )}
-      <form
-        className=" space-y-3"
-        onSubmit={handleSubmit(async (data) => {
-          try {
-            setFormSubmitting(true);
-            await axios.post("/api/issues", data);
-            router.push("/issues");
-            setError("");
-          } catch (error) {
-            setFormSubmitting(false);
-            setError("An unexpected error occurs due to bad request.");
-          }
-        })}
-      >
+      <form className=" space-y-3" onSubmit={onSubmit}>
         <TextField.Root>
           <TextField.Input placeholder="Title" {...register("title")} />
         </TextField.Root>
